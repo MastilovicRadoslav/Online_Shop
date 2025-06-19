cd54d1f6cf956fe4de2aa97d221672fb95fcb85e
Radoslav Mastilović <122049689+MastilovicRadoslav@users.noreply.github.com>
2025-06-19 15:03:30 +0200

    AuthController.cs and AuthService.cs

---
+++ ProductPortalAPI/ProductPortalAPI/Controllers/AuthController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProductPortalAPI.Data;
+using ProductPortalAPI.DTOs;
+using ProductPortalAPI.Models;
+using ProductPortalAPI.Services;
+
+namespace ProductPortalAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        private readonly AuthService _authService;
+
+        public AuthController(AppDbContext context, AuthService authService)
+        {
+            _context = context;
+            _authService = authService; 
+        }
+
+        [HttpPost("register")]
+        public IActionResult Register(UserRegisterDto request)
+        {
+            if (_context.Users.Any(u => u.Username == request.Username))
+                return BadRequest("Korisničko ime već postoji.");
+
+            var user = new User
+            {
+                Username = request.Username,
+                Email = request.Email,
+                PasswordHash = _authService.HashPassword(request.Password),
+                Role = "Customer"
+            };
+
+            _context.Users.Add(user);
+            _context.SaveChanges();
+
+            return Ok("Registracija uspješna");
+
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login(UserLoginDto request)
+        {
+            var user = _context.Users.FirstOrDefault(u =>  u.Username == request.Username);
+            if (user == null)
+                return BadRequest("Korisnik ne postoji.");
+            if (!_authService.VerifyPassword(request.Password, user.PasswordHash))
+                return BadRequest("Pogrešna lozinka.");
+
+            var token = _authService.CreateToken(user);
+            return Ok(new {token});
+        }
+    }
+}
---
+++ ProductPortalAPI/ProductPortalAPI/Services/AuthService.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using ProductPortalAPI.Models;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace ProductPortalAPI.Services
+{
+    public class AuthService
+    {
+        private readonly IConfiguration _configuration;
+
+        public AuthService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string HashPassword(string password)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(password);
+        }
+
+        public bool VerifyPassword(string password, string hash)
+        {
+            return BCrypt.Net.BCrypt.Verify(password, hash);
+        }
+
+        public string CreateToken(User user)
+        {
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                new Claim(ClaimTypes.Name, user.Username),
+                new Claim(ClaimTypes.Role, user.Role)
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(
+                _configuration["Jwt:Key"]!));
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                claims: claims,
+                expires: DateTime.Now.AddDays(1),
+                signingCredentials: creds);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
