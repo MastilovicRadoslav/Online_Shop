60581ad516b083fb943d4af68e094df1bc13b843
Radoslav Mastilović <122049689+MastilovicRadoslav@users.noreply.github.com>
2025-06-19 15:00:21 +0200

    Program.cs and appsettings.json

---
+++ ProductPortalAPI/ProductPortalAPI/Program.cs
@@ -0,0 +1,55 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using ProductPortalAPI.Data;
+using ProductPortalAPI.Services;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+Console.WriteLine("JWT KEY: " + builder.Configuration["Jwt:Key"]);
+
+
+// Dodaj konekciju sa bazom
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Dodaj AuthService kao DI
+builder.Services.AddScoped<AuthService>();
+
+// Dodaj autentifikaciju sa JWT
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = false,
+            ValidateAudience = false,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]!))
+        };
+    });
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Middleware redosled je bitan
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+// Dodaj autentifikaciju i autorizaciju
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
