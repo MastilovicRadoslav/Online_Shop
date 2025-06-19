e86b7386829ad0e3e5e5584464a85074d346cf7b
Radoslav Mastilović <122049689+MastilovicRadoslav@users.noreply.github.com>
2025-06-19 17:51:05 +0200

    Migration

--- ProductPortalAPI/ProductPortalAPI/Data/AppDbContext.cs
+++ ProductPortalAPI/ProductPortalAPI/Data/AppDbContext.cs
@@ -8,5 +8,6 @@ namespace ProductPortalAPI.Data
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
         public DbSet<User> Users { get; set; }
+        public DbSet<Product> Products => Set<Product>();
     }
 }
