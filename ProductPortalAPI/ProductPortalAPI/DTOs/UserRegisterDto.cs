4fe8a603231a3014cf9dc3850ec022aeb2005bf8
Radoslav Mastilović <122049689+MastilovicRadoslav@users.noreply.github.com>
2025-06-19 15:02:02 +0200

    UserLoginDto.cs and UserRegisterDto.cs

---
+++ ProductPortalAPI/ProductPortalAPI/DTOs/UserRegisterDto.cs
@@ -0,0 +1,8 @@
+﻿namespace ProductPortalAPI.DTOs;
+
+public class UserRegisterDto
+{
+    public string Username { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+}
